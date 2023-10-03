71be0175656c64e95d75ad91d354f0664d75bc47
Paul Gradie, PhD <paul.e.gradie@gmail.com>
2023-10-03 12:34:28 +1100

    Update profile.ps1

--- WindowsTerminal/profile.ps1
+++ WindowsTerminal/profile.ps1
@@ -2,16 +2,15 @@ $UserSettings = $env:USERPROFILE + "\OneDrive\.SettingsAndConfiguration"
 . $UserSettings/WindowsTerminal/Functions/functions.ps1
 . $UserSettings/WindowsTerminal/Aliases/CommonAliases.ps1
 
-Import-Module posh-git
-
 # oh-my-posh init pwsh --config 'https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/bubbles.omp.json' | Invoke-Expression
 # oh-my-posh init pwsh --config 'https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/powerlevel10k_modern.omp.json' | Invoke-Expression
 # oh-my-posh init pwsh --config 'https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/tokyonight_storm.omp.json' | Invoke-Expression
 # oh-my-posh init pwsh --config 'https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/tonybaloney.omp.json' | Invoke-Expression
 # oh-my-posh init pwsh --config 'https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/material.omp.json' | Invoke-Expression
 # oh-my-posh init pwsh --config 'https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/marcduiker.omp.json' | Invoke-Expression
-oh-my-posh init pwsh --config 'https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/illusi0n.omp.json' | Invoke-Expression
-
+# oh-my-posh init pwsh --config 'https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/illusi0n.omp.json' | Invoke-Expression
+oh-my-posh init pwsh --config 'https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/material.omp.json' | Invoke-Expression
+# To load these locally, you just need to copy the files down locally and point to theme here.
 
 if($env:COMPUTERNAME -eq "RegEx") {
     . $UserSettings/WindowsTerminal/Aliases/DesktopAliases.ps1
