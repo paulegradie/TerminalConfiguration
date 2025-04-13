b500412cb205b6243f10bb5a48609ea245c5ca6e
paulegradie <paul.e.gradie@gmail.com>
2025-04-14 08:47:23 +1000

    update functions

--- WindowsTerminal/Aliases/GitAliases.ps1
+++ WindowsTerminal/Aliases/GitAliases.ps1
@@ -2,11 +2,38 @@
 try {
     Remove-Alias -Name gcm -Force
     Remove-Alias -Name gp -Force
+    Remove-Alias -Name gcmp -Force
 }
 catch {
     If (Test-Path Alias:gp) { Remove-Item Alias:gp -Force }
     If (Test-Path Alias:gcm) { Remove-Item Alias:gcm -Force }
+    If (Test-Path Alias:gcmp) { Remove-Item Alias:gcmp -Force }
 }
+
+function gcm {
+    param (
+        [string]$commitMessage
+    )
+
+    $branchName = git rev-parse --abbrev-ref HEAD
+    if ($branchName -match "^(\d+)-[a-zA-Z0-9\-]+") {
+        $number = [int]$matches[1]
+
+        if ($number -lt 10000) {
+            $prefix = "PLA-$number"
+        }
+        else {
+            $prefix = "AB#$number"
+        }
+
+        $prefixedMessage = "$prefix $commitMessage"
+        git commit -m $prefixedMessage
+    }
+    else {
+        git commit -m $commitMessage
+    }
+}
+
 
 
 function gco {
@@ -31,23 +58,6 @@ function gpfo {
 
 }
 
-function gpwr {
-    $branch = $args[0]
-
-    if ($branch -eq "") {
-        throw new Error("No branch provided.")
-    }
-
-    if ($branch.ToString().StartsWith("release")) {
-
-    }
-
-    gco master
-    git pull
-    git checkout $branch
-    git rebase master
-    git push origin "$branch"
-}
 
 function gbd {
     git branch -d $args[0]
@@ -61,5 +71,5 @@ function gst { git status }
 function gp { git pull }
 function gb { git branch }
 function gbr { git branch -r }
-function gcm { git commit -m "$args" }
 function gau { git add -u }
+
--- WindowsTerminal/Aliases/LaptopAliases.ps1
+++ WindowsTerminal/Aliases/LaptopAliases.ps1
@@ -1,3 +1,6 @@
+## In powershell, my current understanding is that I need
+## use functions to assign aliases.
+
 ###### General Nav Helpers ##########
 function .. { Set-Location .. }
 function ... { Set-Location ../.. }
@@ -12,3 +15,85 @@ function empower { Set-Location "$Code\empower\empower-app"}
 ############# Personal Location Aliases
 function sail { Set-Location "$Code\Sailfish\"}
 
+# function startup {
+#     wt --title "Palavyr" -d $PORT `; split-pane --title "PDF Service" -d $PDF `; split-pane -H -d $WIDGET
+# }
+
+
+function gohome {
+    $configPath = "$HOME\.ssh\config"
+    $homeIdentity = "    IdentityFile ~/.ssh/home"
+
+    if (Test-Path $configPath) {
+        $content = Get-Content $configPath
+
+        $updatedContent = $content -replace '(?<=Host github\.com\s+HostName github\.com\s+User git\s+IdentityFile )\S+', "$homeIdentity"
+        
+        Set-Content -Path $configPath -Value $updatedContent -Force
+        Write-Host "Switched to home identity file."
+    } else {
+        Write-Host "SSH config file not found."
+    }
+}
+
+function powerup {
+    $configPath = "$HOME\.ssh\config"
+    $empowerIdentity = "    IdentityFile ~/.ssh/empower"
+
+    if (Test-Path $configPath) {
+        $content = Get-Content $configPath
+
+        $updatedContent = $content -replace '(?<=Host github\.com\s+HostName github\.com\s+User git\s+IdentityFile )\S+', "$empowerIdentity"
+        
+        Set-Content -Path $configPath -Value $updatedContent -Force
+        Write-Host "Switched to empower identity file."
+    } else {
+        Write-Host "SSH config file not found."
+    }
+}
+
+
+function Set-GitIdentity {
+    param(
+        [Parameter(Mandatory=$true)]
+        [string]$IdentityType # "home" or "empower"
+    )
+
+    # Define identity details
+    $identities = @{
+        home = @{
+            Name = "paulegradie"
+            Email = "paulegradie@domain.com"
+            HostAlias = "github.com-home"
+            IdentityFile = "~/.ssh/home"
+        }
+        empower = @{
+            Name = "empowerPaul"
+            Email = "paul@empower.me"
+            HostAlias = "github.com-empower"
+            IdentityFile = "~/.ssh/empower"
+        }
+    }
+
+    if (-not $identities.ContainsKey($IdentityType)) {
+        Write-Host "Invalid identity type. Please use 'home' or 'empower'."
+        return
+    }
+
+    $identity = $identities[$IdentityType]
+
+    # Get the current directory (assumed to be the repository root)
+    $RepoPath = Get-Location
+
+    # Set Git user name and email
+    git config user.name $identity.Name
+    git config user.email $identity.Email
+
+    # Update the remote URL to use the correct SSH key alias
+    $currentRemoteUrl = git remote get-url origin
+    $newRemoteUrl = $currentRemoteUrl -replace "github\.com", $identity.HostAlias
+    git remote set-url origin $newRemoteUrl
+
+    Write-Host "Set Git identity to $IdentityType (`$($identity.Name)` <`$($identity.Email)`>) for repository at $RepoPath."
+    Write-Host "Updated remote URL to use SSH key at $($identity.IdentityFile)."
+}
